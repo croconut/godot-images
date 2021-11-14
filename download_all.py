5982128e18294ccb2b886a5a5ab87f7304c146c0
croconut <marsteve@ucsc.edu>
2021-11-13 20:12:36 -0800

    parallelized

--- download_all.py
+++ download_all.py
@@ -1,13 +1,14 @@
 from bs4 import BeautifulSoup
 import requests
-import subprocess
+from subprocess import Popen
 
 IGNORE_LIST=['../',]
 URL="https://downloads.tuxfamily.org/godotengine/"
 
+ALL_PROCESSES=[]
+
 def get_best_link(add_dict, fname, url):
     page = requests.get(url) 
-    print('parsing ', url, ' ...')  
     data = page.text
     soup = BeautifulSoup(data, features='html.parser')
     links = map(lambda x: x.get('href'), soup.find_all('a'))
@@ -26,7 +27,7 @@ def get_best_link(add_dict, fname, url):
         best_link = url + '/' + best_link
         zip_file_name = best_link.rsplit('/', 1)[-1]
         extracted_file_name = zip_file_name.rsplit('.', 1)[0]
-        subprocess.call(["./download_single.sh", fname, best_link, zip_file_name, extracted_file_name])
+        ALL_PROCESSES.append(Popen(["./download_single.sh", fname, best_link, zip_file_name, extracted_file_name]))
     for sfname, sfurl in folders:
         get_best_link(add_dict, sfname, sfurl)            
 
@@ -48,4 +49,7 @@ def main():
         vdict = top_level_versions[version]
         get_best_link(vdict, version, vdict['base_url'])
         
+    for i in ALL_PROCESSES:
+        i.wait()
+
 main()
--- download_single.sh
+++ download_single.sh
@@ -5,17 +5,10 @@ DL_URL=$2
 ZIP_NAME=$3
 EXTRACTED_FILE_NAME=$4
 
-echo $FINAL_NAME $DL_URL $ZIP_NAME $EXTRACTED_FILE_NAME
-
 mkdir -p /tmp/dl_tmp
 
 cd /tmp/dl_tmp
-
-echo "downloading godot from ${DL_URL} ..."
 yes | wget -q ${DL_URL}
-mkdir -p ~/.cache
-mkdir -p ~/.config/godot
-echo "unzipping ${ZIP_NAME} ..."
 yes | unzip -q ${ZIP_NAME} -d ./
 
 mv $EXTRACTED_FILE_NAME /usr/local/bin/${FINAL_NAME}
